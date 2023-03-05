27cee1daca8cb6b06a06a3f1a544d8bb5e604aa1
Manish Thulung <manish.thulung58@gmail.com>
2023-03-05 16:07:26 +0545

    about page added

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadat: Metadata = {
+  title: "about us",
+};
+export default function page() {
+  return <div>this is about us page</div>;
+}
